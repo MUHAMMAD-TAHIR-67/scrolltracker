ab533f46d66b4b9dc894005857ba7cb342822757
Muhammad Tahir <tahirnaeem036@gmail.com>
2026-07-14 23:32:35 +0500

    Initial commit

--- android/app/src/main/java/com/scrolltracker/MainActivity.kt
+++ android/app/src/main/java/com/scrolltracker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.scrolltracker.app
+package com.scrolltracker
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/scrolltracker/MainApplication.kt
+++ android/app/src/main/java/com/scrolltracker/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.scrolltracker.app
+package com.scrolltracker
 
 import android.app.Application
 import android.content.res.Configuration
@@ -14,7 +14,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
 
 import expo.modules.ApplicationLifecycleDispatcher
-import com.scrolltracker.ScrollTrackerPackage
 import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
@@ -25,7 +24,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              add(ScrollTrackerPackage())
+              // add(MyReactNativePackage())
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
